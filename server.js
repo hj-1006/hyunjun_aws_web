857692e992b8cc5740ff2cca7edbca83da85f2b3
hj-1006 <jhj1341012@naver.com>
2026-07-07 15:08:14 +0900

    ci: Update port 80

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = 1006;
+const PORT = 80;
 let count = 0;
 
 const app = express();
